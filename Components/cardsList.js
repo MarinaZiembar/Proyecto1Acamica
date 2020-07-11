deea5b6b146f82e8a03c6a16dbaf9a129b2aed56
Marina Ziembar <marinaziembar@gmail.com>
2020-07-11 01:14:19 -0300

    Logica de filtros

--- Components/cardsList.js
+++ Components/cardsList.js
@@ -2,14 +2,22 @@ import Cards from './cards.js';
 import hotelsData from './data.js';
 
 
-const CardsList = props =>{
+const CardsList = props => {
+
+        let nuevoArray = hotelsData.map(card => (<Cards name={card.name} city={card.city} country={card.country} rooms={card.rooms} price={card.price} lon={card.lon} lat={card.lat} photo={card.photo} description={card.description} />));
+
+        let filters ={
+            bydate:"",
+            bycountry:"",
+            byrooms:"",
+            byprice:"",
+        };
+
         return(
             <div className="cardsList">
-                {
-                hotelsData.map(card => (<Cards name={card.name} city={card.city} country={card.country} rooms={card.rooms} price={card.price} lon={card.lon} lat={card.lat} photo={card.photo} description={card.description} />))
-                }
+                {nuevoArray}
             </div>
-        )
+            )
 };
 
 export default CardsList;
--- Components/select.js
+++ Components/select.js
@@ -4,7 +4,7 @@ const Select = props =>{
     return (
         <div>
             <select onChange={props.handleInputChange} name={props.name} id={props.name}>
-            <option value={props.name} selected disabled>Cualquier {props.name}</option>
+            <option value={props.name} selected>Cualquier {props.name}</option>
             <option value={props.first}>{props.first}</option>
             <option value={props.second}>{props.second}</option>
             <option value={props.third}>{props.third}</option>
